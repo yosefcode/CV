3434e21e19eab6829a99adb2cddac5f34709d0e6
yosefcode <67313446+yosefcode@users.noreply.github.com>
2023-01-31 00:53:31 +0200

    change vercel

--- src/components/slider/apartments/apartments.js
+++ src/components/slider/apartments/apartments.js
@@ -8,7 +8,7 @@ const Shops = () => {
     <div
       className="divall apartments"
       onClick={() => {
-        window.open("https://apartment-wvrh.onrender.com/");
+        window.open("https://apartments-nine.vercel.app/");
       }}
     >
       <div className="title" dir="rtl">
